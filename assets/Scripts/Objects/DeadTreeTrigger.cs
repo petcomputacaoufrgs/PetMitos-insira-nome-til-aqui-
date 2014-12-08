b7610bb34d589b946bf0eb30913e9d61c92264cb
Leonardo <lfilipebsc@gmail.com>
2014-12-08 15:07:33 -0200

    v.1.1. Atualização da primeira versão, indicando em .docx o que se percebeu até aqui de algoritmos necessários exclusivamente no escopo da fase 01.

---
+++ assets/Scripts/Objects/DeadTreeTrigger.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeadTreeTrigger : MonoBehaviour {
+
+	public GameObject detachPiece;
+
+	bool isAttached = true;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		//this.rigidbody2D.isKinematic = false;
+
+		//.Console.WriteLine("Funciona");
+
+		//Debug.Log("Funciona");
+	}
+
+	/*void OnTriggerEnter(Collider other) {
+		//this.rigidbody2D.isKinematic = false;
+
+		//detachPiece.AddComponent<Rigidbody2D>();
+
+
+		//System.Console.WriteLine("Ativou");
+	}*/
+
+	void OnTriggerEnter2D(Collider2D other){
+		//Debug.Log("Funciona");
+
+		//this.GetComponentInParent(GameObject).add
+
+		if(isAttached){
+			isAttached = false;
+			detachPiece.AddComponent<Rigidbody2D>();
+			Debug.Log("Ativou");
+		}
+
+	}
+
+
+
+
+}
---
+++ assets/Third-part/TinyBackground2D/Scripts/Move.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour {
+    public float speed = 5f;
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+        Vector3 pos = transform.position;
+        pos.x += Time.deltaTime * speed;
+        if (pos.x > 10)
+        {
+            pos.x = -10;
+        }
+        transform.position = pos;
+	}
+}
